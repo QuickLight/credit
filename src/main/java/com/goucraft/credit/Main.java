5a4ed6c6b505342da4cbd5e601597c342cf2e1b0
BuildTools <unconfigured@null.spigotmc.org>
2020-02-14 17:26:45 +0800

    Initial commit

---
+++ src/main/java/com/goucraft/credit/Main.java
@@ -0,0 +1,20 @@
+package com.goucraft.credit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+private static Main instance;
+//private static CreditAPI creditAPI;
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+
+}
